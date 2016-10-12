abc0ea3517380084b3792e0482f6af197acba440
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-12 21:22:11 +0000

    Second version now working!

--- scripted_http_request.js
+++ scripted_http_request.js
@@ -1,17 +1,16 @@
 var request = require("request");
 
 function readHTML (url, callback){
-  callback(request(url));
+  request(url, callback);
   }
-}
-}
 
-function getBody (error, response, body)){
+
+function getBody (error, response, body){
   if(!error && response.statusCode === 200){
     console.log(body);
   }
   else {
     console.log(error);
   }
-
+}
 readHTML('http://kimberleydillon.github.io/', getBody);
