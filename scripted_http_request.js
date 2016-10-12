b43c9453a97332bcfef87a21292946c05e65ac15
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-12 21:25:03 +0000

    third version with comments

--- scripted_http_request.js
+++ scripted_http_request.js
@@ -1,16 +1,16 @@
-var request = require("request");
+var request = require("request"); //gets require module
 
-function readHTML (url, callback){
-  request(url, callback);
+function readHTML (url, callback){ //intial function called
+  request(url, callback); //requests url then asychronously applies cb function
   }
 
 
-function getBody (error, response, body){
-  if(!error && response.statusCode === 200){
+function getBody (error, response, body){ // cb function to print body
+  if(!error && response.statusCode === 200){//if no error and ok status proceed
     console.log(body);
   }
-  else {
+  else { // if not log error
     console.log(error);
   }
 }
-readHTML('http://kimberleydillon.github.io/', getBody);
+readHTML('http://kimberleydillon.github.io/', getBody); //calling main function with url and cb.
